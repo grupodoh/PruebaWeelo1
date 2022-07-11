86d4bf1e5e3d02dcc90ac2514e38186ffb8fc634
Juan Diego Moncaleano Quiñones <Juan.Moncaleano@interedes.local>
2022-07-11 15:17:00 -0500

    Se agrega el archivo de los casos prueba.

--- index.js
+++ index.js
@@ -6,33 +6,33 @@ async function main(){
     
     let driver = await ConfigAutomaticTest.initConfig()
 
-    // await(new RegistroFormulario(driver).CP_1());
+    await(new RegistroFormulario(driver).CP_1());
 
-    // await(new RegistroFormulario(driver).CP_2());
+    await(new RegistroFormulario(driver).CP_2());
 
-    // await(new RegistroFormulario(driver).CP_3());
+    await(new RegistroFormulario(driver).CP_3());
 
-    // await(new RegistroFormulario(driver).CP_4());
+    await(new RegistroFormulario(driver).CP_4());
 
-    // await(new RegistroFormulario(driver).CP_5());
+    await(new RegistroFormulario(driver).CP_5());
 
-    // await(new RegistroFormulario(driver).CP_6());
+    await(new RegistroFormulario(driver).CP_6());
 
-    // await(new RegistroFormulario(driver).CP_7());
+    await(new RegistroFormulario(driver).CP_7());
 
-    //  await(new RegistroFormulario(driver).CP_8());
+    await(new RegistroFormulario(driver).CP_8());
 
-    // await(new RegistroFormulario(driver).CP_9());
+    await(new RegistroFormulario(driver).CP_9());
 
-    // await(new RegistroFormulario(driver).CP_10());
+    await(new RegistroFormulario(driver).CP_10());
 
-    // await(new RegistroFormulario(driver).CP_11());
+    await(new RegistroFormulario(driver).CP_11());
 
-    // await(new RegistroFormulario(driver).CP_12());
+    await(new RegistroFormulario(driver).CP_12());
 
-    // await(new RegistroFormulario(driver).CP_13());
+    await(new RegistroFormulario(driver).CP_13());
 
-    // await(new RegistroFormulario(driver).CP_14());
+    await(new RegistroFormulario(driver).CP_14());
 
     await(new RegistroFormulario(driver).CP_15());
 
